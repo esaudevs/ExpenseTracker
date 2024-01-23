afdb03e8e735e5e08465a362f4a94a37f532f262
Esau <alan.garcia@citi.com.mx>
2024-01-23 14:16:39 -0600

    [tracker] dispatchers injection

--- app/src/main/java/com/esaudev/expensetracker/data/repository/DefaultExpenseRepository.kt
+++ app/src/main/java/com/esaudev/expensetracker/data/repository/DefaultExpenseRepository.kt
@@ -5,14 +5,17 @@ import com.esaudev.expensetracker.data.local.database.model.toExpense
 import com.esaudev.expensetracker.data.local.database.model.toExpenseEntity
 import com.esaudev.expensetracker.domain.model.Expense
 import com.esaudev.expensetracker.domain.repository.ExpenseRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import javax.inject.Named
 
 class DefaultExpenseRepository @Inject constructor(
-    private val expenseDao: ExpenseDao
+    private val expenseDao: ExpenseDao,
+    @Named("io") private val dispatcher: CoroutineDispatcher,
 ) : ExpenseRepository {
     override fun observeByMonth(monthValue: String): Flow<List<Expense>> {
         return expenseDao.observeByMonth(monthValue = monthValue).map { expenseEntityList ->
@@ -27,7 +30,7 @@ class DefaultExpenseRepository @Inject constructor(
     }
 
     override suspend fun deleteById(id: String) {
-        withContext(Dispatchers.IO) {
+        withContext(dispatcher) {
             expenseDao.deleteById(id)
         }
     }
---
+++ app/src/main/java/com/esaudev/expensetracker/di/DispatcherModule.kt
@@ -0,0 +1,20 @@
+package com.esaudev.expensetracker.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+    @Singleton
+    @Provides
+    @Named("io")
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
